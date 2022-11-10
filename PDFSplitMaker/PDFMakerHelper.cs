0bccc3e810ac63467d9dc12c1b1770c6790be53b
Agni Gonçalves Botelho Campos <agni.campos@radixeng.com.br>
2022-11-09 22:29:10 -0300

    1st commit

---
+++ PDFSplitMaker/PDFMakerHelper.cs
@@ -0,0 +1,32 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+
+namespace PDFSplitMaker
+{
+    public static class PDFMakerHelper
+    {
+        public static void PdfMaker(string origin, string destiny, int[] pages)
+        {
+            if (!File.Exists(origin))
+                throw new Exception("File not found!");
+
+            var doc = PdfReader.Open(origin, PdfDocumentOpenMode.Import);
+            var newDoc = new PdfDocument();
+            int currentPage = 1;
+
+            foreach (var page in doc.Pages)
+            {
+                if (pages.Any(pg => pg == currentPage))
+                    newDoc.AddPage(page);
+
+                currentPage++;
+            }
+
+            if (newDoc.Pages.Count == 0)
+                throw new Exception("No pages!");
+
+            newDoc.Save(destiny);
+
+        }
+    }
+}
---
+++ PDFSplitMaker/Program.cs
@@ -0,0 +1,207 @@
+﻿
+using PDFSplitMaker;
+using System.Reflection;
+
+string path = Environment.CurrentDirectory.Replace("PDFSplitMaker\\bin\\Debug\\net6.0", "files");
+var origin = @$"{path}\OOGTK1-04-L-26-XN-00001-001-X0-5-001.pdf";
+var destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+
+int[] pdf1 = { 5, 6 };
+PDFMakerHelper.PdfMaker(origin, destiny, pdf1);
+Thread.Sleep(2000); 
+
+int[] pdf2 = { 7, 8 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf2);
+Thread.Sleep(2000);
+
+int[] pdf3 = { 9 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf3);
+Thread.Sleep(2000);
+
+int[] pdf4 = { 10, 11 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf4);
+Thread.Sleep(2000);
+
+int[] pdf5 = { 12, 13 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf5);
+Thread.Sleep(2000);
+
+int[] pdf6 = { 14, 14 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf6);
+Thread.Sleep(2000);
+
+int[] pdf7 = { 15, 15 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf7);
+Thread.Sleep(2000);
+
+int[] pdf8 = { 16, 17 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf8);
+Thread.Sleep(2000);
+
+int[] pdf9 = { 18, 19 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf9);
+Thread.Sleep(2000);
+
+int[] pdf10 = { 20, 20 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf10);
+Thread.Sleep(2000);
+
+int[] pdf11 = { 21, 21 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf11);
+Thread.Sleep(2000);
+
+int[] pdf12 = { 22, 22 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf12);
+Thread.Sleep(2000);
+
+int[] pdf13 = { 23, 23 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf13);
+Thread.Sleep(2000);
+
+int[] pdf14 = { 24, 24 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf14);
+Thread.Sleep(2000);
+
+int[] pdf15 = { 25, 25 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf15);
+Thread.Sleep(2000);
+
+int[] pdf16 = { 26, 26 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf16);
+Thread.Sleep(2000);
+
+int[] pdf17 = { 27, 27 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf17);
+Thread.Sleep(2000);
+
+int[] pdf18 = { 28, 28 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf18);
+Thread.Sleep(2000);
+
+int[] pdf19 = { 29, 31 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf19);
+Thread.Sleep(2000);
+
+int[] pdf20 = { 32, 35 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf20);
+Thread.Sleep(2000);
+
+int[] pdf21 = { 36 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf21);
+Thread.Sleep(2000);
+
+int[] pdf22 = { 37 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf22);
+Thread.Sleep(2000);
+
+int[] pdf23 = { 38, 38 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf23);
+Thread.Sleep(2000);
+
+int[] pdf24 = { 39, 40 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf24);
+Thread.Sleep(2000);
+
+int[] pdf25 = { 41 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf25);
+Thread.Sleep(2000);
+
+int[] pdf26 = { 43 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf26);
+Thread.Sleep(2000);
+
+int[] pdf27 = { 44 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf27);
+Thread.Sleep(2000);
+
+int[] pdf28 = { 45 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf28);
+Thread.Sleep(2000);
+
+int[] pdf29 = { 46 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf29);
+Thread.Sleep(2000);
+
+int[] pdf30 = { 47 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf30);
+Thread.Sleep(2000);
+
+int[] pdf31 = { 48 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf31);
+Thread.Sleep(2000);
+
+int[] pdf32 = { 49 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf32);
+Thread.Sleep(2000);
+
+int[] pdf33 = { 50 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf33);
+Thread.Sleep(2000);
+
+int[] pdf34 = { 51, 56 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf34);
+Thread.Sleep(2000);
+
+int[] pdf35 = { 57 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf35);
+Thread.Sleep(2000);
+
+int[] pdf36 = { 58 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf36);
+Thread.Sleep(2000);
+
+int[] pdf37 = { 59 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf37);
+Thread.Sleep(2000);
+
+int[] pdf38 = { 60 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf38);
+Thread.Sleep(2000);
+
+int[] pdf39 = { 61 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf39);
+Thread.Sleep(2000);
+
+int[] pdf40 = { 62 };
+destiny = @$"{path}\tools{DateTime.Now.ToString("yyyy-MM-dd.hhmmsss")}.pdf";
+PDFMakerHelper.PdfMaker(origin, destiny, pdf40);
+
+
